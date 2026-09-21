29f7a66c91f7f80dc6bbfe409d8cc19617d07392
otaviosilvavendas <otaviosilva.vendas@gmail.com>
2026-09-21 17:36:19 -0300

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,2 +1,12 @@
-import {createClient as createSupabaseClient} from '@supabase/supabase-js';
-export function createClient(){return createSupabaseClient(process.env.NEXT_PUBLIC_SUPABASE_URL!,(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY)!)}
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+
+export function createClient() {
+  return createSupabaseClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    (
+      process.env.SUPABASE_SERVICE_ROLE_KEY ||
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+    )!
+  );
+}
